d8b19c698faf71627e5d60187dfc55b70c9afe94
Matthew Miller <Matthewjmiller07@gmail.com>
2023-10-24 17:10:28 -0500

    Update script.js
    Making blanks numbered based on blank

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let correctWords = [];
 let fullVerse = '';  // Global variable to hold the full verse
+let blankNumber = 0;  // Global variable to keep track of the blank number
 
 async function fetchText() {
     let textInput = document.getElementById('text-input').value;
@@ -14,7 +15,6 @@ async function fetchText() {
 
     if (Array.isArray(data.he)) {
         if (isVerseLevel) {
-            // Adjusted to handle verse ranges
             const verseData = data.he.flat();  // Flatten the array
             return verseData.join(' ');  // Join array of strings into a single string for verse-level query
         } else {
@@ -52,6 +52,7 @@ function getBlankInterval() {
 }
 
 function startGame() {
+    blankNumber = 0;  // Reset the blank number at the start of each game
     fetchText().then(text => {
         const strippedText = stripHebrew(text);
         const words = strippedText.split(' ');
@@ -60,7 +61,8 @@ function startGame() {
         const blankInterval = getBlankInterval();  // Get the blank interval based on difficulty
         words.forEach((word, index) => {
             if ((index + 1) % blankInterval === 0) {
-                gameContent += `<input type="text" class="blank" data-index="${index}" placeholder="${index + 1}" /> `;
+                blankNumber++;  // Increment the blank number
+                gameContent += `<input type="text" class="blank" data-index="${index}" /> `;
                 correctWords.push(word);
             } else {
                 gameContent += word + ' ';
@@ -76,22 +78,19 @@ function startGame() {
 function checkAnswers() {
     const blanks = document.querySelectorAll('.blank');
     let score = 0;
-    let blankIndex = 0;
     let comparisonContent = '';
-    blanks.forEach(blank => {
-        const blankNumber = blankIndex + 1;
-        if (blank.value === correctWords[blankIndex]) {
-            score++;
-            comparisonContent += `<p>Blank #${blankNumber} - Correct: Your Answer: ${blank.value}, Correct Answer: ${correctWords[blankIndex]}</p>`;
-        } else {
-            comparisonContent += `<p>Blank #${blankNumber} - Incorrect: Your Answer: ${blank.value}, Correct Answer: ${correctWords[blankIndex]}</p>`;
-        }
+    blanks.forEach((blank, index) => {
+        const blankNumber = index + 1;
         blank.setAttribute('placeholder', `Blank #${blankNumber}`);  // Set the placeholder to show the blank number
         blank.disabled = true;  // Optionally disable the input to prevent further changes
-        blankIndex++;
+        if (blank.value === correctWords[index]) {
+            score++;
+            comparisonContent += `<p style="color: green;">Blank #${blankNumber} - Correct: Your Answer: ${blank.value}, Correct Answer: ${correctWords[index]}</p>`;
+        } else {
+            comparisonContent += `<p style="color: red;">Blank #${blankNumber} - Incorrect: Your Answer: ${blank.value}, Correct Answer: ${correctWords[index]}</p>`;
+        }
     });
     document.getElementById('score').innerText = `Score: ${score} out of ${blanks.length}`;
     document.getElementById('comparison-container').innerHTML = comparisonContent;
     document.getElementById('full-verse-container').innerText = fullVerse;
 }
-
